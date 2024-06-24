53233c80879338bbc77016316617bde1e0296962
JohnMichaelWoodrow <144362999+JohnMichaelWoodrow@users.noreply.github.com>
2024-06-24 10:34:32 -0230

    Delete Assignment3 directory

--- Assignment3/app/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { registerRootComponent } from 'expo';
-import HomeLayout from './layout'; // Correct import path
-
-const App = () => <HomeLayout />;
-
-registerRootComponent(App); // Ensure this registers the main component
